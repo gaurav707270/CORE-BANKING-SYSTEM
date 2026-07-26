d9b1d192225096c44e77b6fcda6787317d44f606
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-07-26 21:17:07 +0530

    ..

---
+++ Backend/models/user_model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required for creating a user !"],
+    trim: true,
+    lowercase: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address","Invalid Email address."
+    ],
+  },
+  
+});
