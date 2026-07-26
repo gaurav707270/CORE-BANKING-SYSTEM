11dabfca761c53560ceccb8b7d23ed2d4e5c0536
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-07-26 21:02:40 +0530

    ..

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
-const connectDB = async () => {
+
+const connectToDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/bankingSys");
-    console.log("MongoDB connected successfully !!");
+    await mongoose.connect(process.env.MONGO_URI);//MONGO_URI that is come for .env file for url 
+    console.log("MongoDB Connected Successfully !!");
   } catch (err) {
-    console.log("Mongodb connection failes ! ", err);
+    console.error("Error connecting to MongoDB !!", err.message);
+    process.exit(1);
   }
 };
-export default connectDB;
+
+export default connectToDB;
+
+//const connectDB = async () => {
+//  try {
+//    await mongoose.connect("mongodb://127.0.0.1:27017/bankingSys");
+//    console.log("MongoDB connected successfully !!");
+//  } catch (err) {
+//    console.log("Mongodb connection failes ! ", err);
+//  }
+//};
+//export default connectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,32 @@
-//server start here
-import app from "./app.js";
-import mongosse from 'mongosse'
+////server start here
+//require("dotenv").config()
+//
+//import app from "./app.js";
+//import connectToDB from './config/db.js'
+////import mongosse from 'mongosse'
+//
+////function call ===> config => db.js => one function create (ConnectToDB)  => that is caling  
+//connectToDB()
+//app.listen(3000, () => {
+//    console.log("Server running on port 3000");
+//});
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+
+
+//Ye project ka starting point hai.
+import dotenv from "dotenv";
+dotenv.config();//.env file ko read karta hai.
+
+import app from "./app.js";
+import connectToDB from "./config/db.js";
+
+const PORT = process.env.PORT || 3000;
+
+///function call ===> config => db.js => one function create (ConnectToDB)  => that is caling 
+//Ye function call hua.
+//Iske andar MongoDB connect hota hai.
+connectToDB();
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
