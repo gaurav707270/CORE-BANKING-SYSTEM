f4110409b1afcfbfe97b104eee456ff5dc689932
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-08-05 23:36:34 +0530

    ..

--- Backend/models/user_model.js
+++ Backend/models/user_model.js
@@ -37,8 +37,6 @@ const userSchema = new mongoose.Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     return next();
-
-    
   }
   const hash = await bcrypt.hash(this.password, 10);
   this.password = hash;
